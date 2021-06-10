973807a5285e49272d99f58a6a60524cca889844
Thaweesak Saiwongse <Thaweesak_sai@hotmail.com>
2021-06-11 00:28:06 +0700

    add itemlist

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,34 @@
+import { EditOutlined, EllipsisOutlined } from '@ant-design/icons';
 import Icon from '@ant-design/icons/lib/components/Icon';
-import {
-  EditOutlined,
-  EllipsisOutlined,
-  SettingOutlined
-} from '@ant-design/icons';
-import { Avatar, Card, Col, Row } from 'antd';
+import { Avatar, Badge, Card, Col, Row } from 'antd';
 import Meta from 'antd/lib/card/Meta';
 import CSS from 'csstype';
 import React from 'react';
 import '../App.css';
+
+const itemList = [
+  {
+    id: 1,
+    itemName: 'bag',
+    description: 'description1',
+    imgURL:
+      'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png'
+  },
+  {
+    id: 2,
+    itemName: 'Hat',
+    description: 'description2',
+    imgURL:
+      'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png'
+  },
+  {
+    id: 3,
+    itemName: 'Bed',
+    description: 'description3',
+    imgURL:
+      'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png'
+  }
+];
 
 const App: React.FC = () => {
   const gridStyle: CSS.Properties = {
@@ -21,81 +41,179 @@ const App: React.FC = () => {
   };
 
   return (
-    <Card>
+    <div>
       <Row gutter={16}>
-        <Col className="gutter-row" span={8}>
-          <Card
-            cover={
-              <img
-                alt="example"
-                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-              />
-            }
-            actions={[
-              <Icon type="setting" />,
-              <Icon type="edit" />,
-              <Icon type="ellipsis" />
-            ]}
-          >
-            <Meta
-              avatar={
-                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-              }
-              title="Card title"
-              description="This is the description"
-            />
-          </Card>
-        </Col>
-
-        <Col className="gutter-row" span={8}>
-          <Card
-            cover={
-              <img
-                alt="example"
-                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-              />
-            }
-            actions={[
-              // <SettingOutlined key="setting" />,
-              <EditOutlined key="edit" />,
-              <EllipsisOutlined key="ellipsis" />
-            ]}
-          >
-            <Meta
-              avatar={
-                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-              }
-              title="Card title"
-              description="This is the description"
-            />
-          </Card>
-        </Col>
-        <Col className="gutter-row" span={8}>
-          <Card
-            cover={
-              <img
-                alt="example"
-                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-              />
-            }
-            actions={[
-              <Icon type="setting" />,
-              <Icon type="edit" />,
-              <Icon type="ellipsis" />
-            ]}
-          >
-            <Meta
-              avatar={
-                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-              }
-              title="Card title"
-              description="This is the description"
-            />
-          </Card>
-        </Col>
+        {itemList.map((item) => {
+          console.log(item.imgURL);
+          return (
+            <Col className="gutter-row" span={8}>
+              <Card
+                hoverable={true}
+                cover={<img src={item.imgURL} />}
+                actions={[
+                  <EditOutlined
+                    key="edit"
+                    onClick={() => {
+                      console.log('click on ', item.itemName);
+                    }}
+                  />,
+                  <EllipsisOutlined key="ellipsis" />
+                ]}
+              >
+                <Meta
+                  title={item.itemName}
+                  description={item.description}
+                ></Meta>
+              </Card>
+            </Col>
+          );
+        })}
       </Row>
-    </Card>
+    </div>
   );
 };
+// return (
+//   <Card>
+//     <Row gutter={16}>
+//       <Col className="gutter-row" span={8}>
+//         <Card
+//           cover={
+//             <img src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />
+//           }
+//           actions={[
+//             <Icon type="setting" />,
+//             <Icon type="edit" />,
+//             <Icon type="ellipsis" />
+//           ]}
+//         >
+//           <Meta
+//             avatar={
+//               <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+//             }
+//             title="Card title"
+//             description="This is the description"
+//           />
+//         </Card>
+//       </Col>
+
+//       <Col className="gutter-row" span={8}>
+//         <Card
+//           cover={
+//             <img
+//               alt="example"
+//               src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+//             />
+//           }
+//           actions={[
+//             // <SettingOutlined key="setting" />,
+//             <EditOutlined key="edit" />,
+//             <EllipsisOutlined key="ellipsis" />
+//           ]}
+//         >
+//           <Meta
+//             avatar={
+//               <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+//             }
+//             title="Card title"
+//             description="This is the description"
+//           />
+//         </Card>
+//       </Col>
+//       <Col className="gutter-row" span={8}>
+//         <Card
+//           cover={
+//             <img
+//               alt="example"
+//               src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+//             />
+//           }
+//           actions={[
+//             <Icon type="setting" />,
+//             <Icon type="edit" />,
+//             <Icon type="ellipsis" />
+//           ]}
+//         >
+//           <Meta
+//             avatar={
+//               <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+//             }
+//             title="Card title"
+//             description="This is the description"
+//           />
+//         </Card>
+//       </Col>
+//     </Row>
+//     <Row gutter={16}>
+//       <Col className="gutter-row" span={8}>
+//         <Card
+//           cover={
+//             <img
+//               alt="example"
+//               src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+//             />
+//           }
+//           actions={[
+//             // <SettingOutlined key="setting" />,
+//             <EditOutlined key="edit" />,
+//             <EllipsisOutlined key="ellipsis" />
+//           ]}
+//         >
+//           <Meta
+//             avatar={
+//               <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+//             }
+//             title="Card title"
+//             description="This is the description"
+//           />
+//         </Card>
+//       </Col>
+//       <Col className="gutter-row" span={8}>
+//         <Card
+//           cover={
+//             <img
+//               alt="example"
+//               src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+//             />
+//           }
+//           actions={[
+//             // <SettingOutlined key="setting" />,
+//             <EditOutlined key="edit" />,
+//             <EllipsisOutlined key="ellipsis" />
+//           ]}
+//         >
+//           <Meta
+//             avatar={
+//               <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+//             }
+//             title="Card title"
+//             description="This is the description"
+//           />
+//         </Card>
+//       </Col>
+//       <Col className="gutter-row" span={8}>
+//         <Card
+//           cover={
+//             <img
+//               alt="example"
+//               src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+//             />
+//           }
+//           actions={[
+//             // <SettingOutlined key="setting" />,
+//             <EditOutlined key="edit" />,
+//             <EllipsisOutlined key="ellipsis" />
+//           ]}
+//         >
+//           <Meta
+//             avatar={
+//               <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+//             }
+//             title="Card title"
+//             description="This is the description"
+//           />
+//         </Card>
+//       </Col>
+//     </Row>
+//   </Card>
 
 export default App;
