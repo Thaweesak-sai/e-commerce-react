81c424e4391e6d151806fdd3f77c1fedadd7245a
Thaweesak Saiwongse <Thaweesak_sai@hotmail.com>
2021-06-11 00:13:54 +0700

    orgs import

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../pages/home';
 import ListingPage from '../pages/listing';
